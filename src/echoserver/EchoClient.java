c2276d6055fe51d581b2de676dd79e0ba9a88b68
Tristan Kalvoda <kalvo007@morris.umn.edu>
2024-10-22 11:40:20 -0500

    Initial server architecture created
    Co-authored-by: Dylan Packer <packe089@users.noreply.github.com>

--- src/echoserver/EchoClient.java
+++ src/echoserver/EchoClient.java
@@ -1,4 +1,47 @@
 package echoserver;
 
+import java.net.*;
+import java.nio.Buffer;
+import java.io.*;
+
 public class EchoClient {
+    public static final int portNumber = 6013;
+
+    public static void main(String[] args) {
+        String server;
+        // Use "127.0.0.1", i.e., localhost, if no server is specified.
+        if (args.length == 0) {
+            server = "127.0.0.1";
+        } else {
+            server = args[0];
+        }
+
+        try {
+            // connect to server
+            Socket socket = new Socket(server, portNumber);
+
+            // Get the input stream so we can read from that socket
+            InputStream input = socket.getInputStream();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+
+            // Print all input received from server
+            String line;
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+            socket.close();
+
+            // provide some minimal error handling
+        } catch (ConnectException ce) {
+            System.out.println("We were unable to connect to " + server);
+            System.out.println("You should make sure the server is running.");
+        } catch (IOException ioe) {
+            System.out.println("We caught an unexpected exception");
+            System.err.println(ioe);
+        }
+    }
 }
+
+// [kalvo007@moonbus src]$ javac echoserver/EchoClient.java 
+// [kalvo007@moonbus src]$ java echoserver.EchoClient 
+// Tue Oct 22 11:36:27 CDT 2024
--- src/echoserver/EchoServer.java
+++ src/echoserver/EchoServer.java
@@ -1,4 +1,38 @@
 package echoserver;
 
+import java.io.*;
+import java.net.*;
+
 public class EchoServer {
+    public static final int portNumber = 6013;
+    
+    public static void main(String[] args) {
+        try {
+            // Start listening on portNumber
+            ServerSocket sock = new ServerSocket(portNumber);
+
+            while (true) {
+                // Wait until someone accepts
+                Socket client = sock.accept();
+                System.out.println("Got request!");
+
+                // Construct a writer so we can write to the socket, thereby
+                // sending something back to the client.
+                PrintWriter writer = new PrintWriter(client.getOutputStream(), true);
+
+                // Send date back to the client
+                writer.println(new java.util.Date().toString());
+
+                // Close socket when done
+                client.close();
+            }
+        } catch (IOException ioe) {
+            System.out.println("We caught an unexpected exception");
+            System.err.println(ioe);
+        }
+    }
 }
+
+// [kalvo007@moonbus echoserver]$ javac -d out EchoServer.java
+// [kalvo007@moonbus echoserver]$ java -cp out echoserver.EchoServer
+// Got request!
