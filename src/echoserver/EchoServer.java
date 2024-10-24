08221650cfdbecd6a93f6dc7d122bcaf73acb810
Tristan Kalvoda <kalvo007@morris.umn.edu>
2024-10-24 11:09:05 -0500

    updated EchoServer.java code for lab tasks so it handles inputs and echos
    Co-authored-by: Dylan Packer <packe089@users.noreply.github.com>

--- src/echoserver/EchoServer.java
+++ src/echoserver/EchoServer.java
@@ -16,12 +16,25 @@ public class EchoServer {
                 Socket client = sock.accept();
                 System.out.println("Got request!");
 
+                // Read a single byte from the client
+                //Read(sock.getInp)
+                InputStream input = client.getInputStream();
+                OutputStream output = client.getOutputStream();
+                
+                //int data = input.read();
+                // System.out.println("Received byte:" + data);
+
+                int data;
+                
+                while ((data = input.read()) != -1) {
+                    System.out.println("Received byte:" + data);
+                    output.write(data);
+                }
+
+
                 // Construct a writer so we can write to the socket, thereby
                 // sending something back to the client.
-                PrintWriter writer = new PrintWriter(client.getOutputStream(), true);
-
-                // Send date back to the client
-                writer.println(new java.util.Date().toString());
+                // PrintWriter writer = new PrintWriter(client.getOutputStream(), true);
 
                 // Close socket when done
                 client.close();
